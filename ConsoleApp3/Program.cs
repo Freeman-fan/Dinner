fb371c5ea3554962cb49fdcaaa5c18562b27b1ec
Freeman <freemanfan@qq.com>
2024-03-06 20:46:42 +0800

    添加了m码标注功能。图片接受与回复，图片处理实现。添加了回复内容。

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Sora;
@@ -17,7 +18,7 @@ using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using Sora.Net;
 
-namespace ConsoleApp3
+namespace DinnerBot
 {
     internal class Program
     {
@@ -30,7 +31,16 @@ namespace ConsoleApp3
                 .SetLogLevel(LogLevel.Info);
 
             //全局变量
-            double rate = 0;//汇率
+            double rate = 0; //汇率
+            string[,] memberState = new string[10, 2];  //成员变量初始化
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    memberState[i, j] = null;
+                }
+            }
+            string cn_mNum = "";
 
             /****************
              * 实例化sora服务
@@ -39,25 +49,56 @@ namespace ConsoleApp3
             ISoraService service = SoraServiceFactory.CreateService(new ServerConfig());
 
 
+            /*************
+             * 监听事件
+             *************/
+
             service.Event.OnPrivateMessage += async (sender, EventArgs) =>  //私聊消息处理
             {
-                //判断临时会话
+                /*
+                //判断临时会话，是则退出
                 if (EventArgs.IsTemporaryMessage == true)
                 {
                     return;
                 }
+                */
 
 
-                /*
                 //////////////////////////////////////////////
                 ///图片处理
                 //////////////////////////////////////////////
-
+                /*
                 //图片处理入口
                 if (EventArgs.Message.IsMultiImageMessage() == true || EventArgs.Message.IsSingleImageMessage() == true)
                 {
-                    System.Drawing.Image image = (System.Drawing.Image)EventArgs.Message.GetAllImage();
 
+                    //获取图片(本地路径)
+                    IEnumerable<Sora.Entities.Segment.DataModel.ImageSegment> T = new List<Sora.Entities.Segment.DataModel.ImageSegment>();
+                    T = EventArgs.Message.GetAllImage();
+                    List<Sora.Entities.Segment.DataModel.ImageSegment> imageSegments = new List<Sora.Entities.Segment.DataModel.ImageSegment>();
+                    imageSegments = T.ToList();
+                    string imagePath = imageSegments[0].ImgFile;
+                    imagePath = imagePath.Remove(0, 7);  //移除imgFile路径中的"file://"
+
+                    //图片处理函数
+                    Bitmap bitmap = new Bitmap(imagePath);
+                    Bitmap newBitmap = new Bitmap(bitmap.Width / 2, bitmap.Height / 2);
+                    for (int i = 0; i < newBitmap.Width; i++)
+                    {
+                        for (int j = 0; j < newBitmap.Height; j++)
+                        {
+                            newBitmap.SetPixel(i, j, bitmap.GetPixel(i, j));
+                        }
+                    }
+                    MemoryStream ms = new MemoryStream();
+                    newBitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+
+                    //当state为1.2时，获取图片内容
+
+
+                    //发送消息
+                    MessageBody mb = SayPhoto(ms);
+                    await EventArgs.Sender.SendPrivateMessage(mb);
                 };
                 */
 
@@ -68,9 +109,128 @@ namespace ConsoleApp3
 
                 string rawMessage = EventArgs.Message.GetText();
 
+                //业务二级目录判断
+                for (int i = 0; i < 10; i++)
+                {
+                    //判断是否有业务状态，是则获取状态码
+                    if (memberState[i, 0] == EventArgs.SenderInfo.UserId.ToString())
+                    {
+                        //标cn流程 1.1
+                        switch (memberState[i, 1])
+                        {
+                            case "1.1":
+                                {
+                                    if (EventArgs.Message.GetText() == "结束")
+                                    {
+                                        memberState[i, 0] = null;
+                                        memberState[i, 1] = null;
+                                        MessageBody mb1 = SayTextMessage("已退出标记cn功能");
+                                        await EventArgs.Sender.SendPrivateMessage(mb1);
+                                        break;
+                                    }
+                                    cn_mNum = EventArgs.Message.GetText();
+                                    string[] cn__mNum = cn_mNum.Split("，");
+                                    if (cn__mNum.Length != 2)
+                                    {
+                                        MessageBody mb3 = SayTextMessage("格式有误，请重新输入");
+                                        await EventArgs.Sender.SendPrivateMessage(mb3);
+                                        break;
+                                    }
+                                    memberState[i, 1] = "1.2";
+                                    MessageBody mb2 = SayTextMessage("已收到cn和m码，请发送对应图片。\n发送“重置”清除当前记录");
+                                    await EventArgs.Sender.SendPrivateMessage(mb2);
+                                    break;
+                                }
+                            case "1.2":
+                                {
+                                    if (EventArgs.Message.GetText() == "重置")
+                                    {
+                                        memberState[i, 1] = "1.1";
+                                        MessageBody mb1 = SayTextMessage("已清除当前记录，请重新发送\n发送“结束”退出当前状态");
+                                        await EventArgs.Sender.SendPrivateMessage(mb1);
+                                        break;
+                                    }
+                                    else if (EventArgs.Message.GetText() == "结束")
+                                    {
+                                        memberState[i, 0] = null;
+                                        memberState[i, 1] = null;
+                                        MessageBody mb1 = SayTextMessage("已退出标记cn功能");
+                                        await EventArgs.Sender.SendPrivateMessage(mb1);
+                                        break;
+                                    }
+                                    else if (EventArgs.Message.IsMultiImageMessage() == false && EventArgs.Message.IsSingleImageMessage() == false)
+                                    {
+                                        MessageBody mb2 = SayTextMessage("图片格式错误，请重新发送");
+                                        await EventArgs.Sender.SendPrivateMessage(mb2);
+                                        break;
+                                    }
+                                    MessageBody mb5 = SayTextMessage("已收到图片，正在处理，请稍候");
+                                    await EventArgs.Sender.SendPrivateMessage(mb5);
+                                    //获取图片
+                                    IEnumerable<Sora.Entities.Segment.DataModel.ImageSegment> T = new List<Sora.Entities.Segment.DataModel.ImageSegment>();
+                                    T = EventArgs.Message.GetAllImage();
+                                    List<Sora.Entities.Segment.DataModel.ImageSegment> imageSegments = new List<Sora.Entities.Segment.DataModel.ImageSegment>();
+                                    imageSegments = T.ToList();
+                                    string imagePath = imageSegments[0].ImgFile;
+                                    imagePath = imagePath.Remove(0, 7);  //移除imgFile路径中的"file://"
+                                    //处理图片
+                                    Bitmap image = new Bitmap(imagePath);
+                                    Bitmap newimage = new Bitmap(image.Width, image.Height);
+                                    string[] cn__mNum = cn_mNum.Split("，");
+                                    string cn = cn__mNum[0];
+                                    string mNum = cn__mNum[1];
+                                    /*
+                                    for(int x = 0; x < image.Width; x++)
+                                    {
+                                        for(int y = 0;y < image.Height; y++)
+                                        {
+                                            newimage.SetPixel(x, y, image.GetPixel(x, y));
+                                        }
+                                    }
+                                    */
+                                    Graphics graphics = Graphics.FromImage(image);
+                                    Brush brush = new SolidBrush(Color.White);
+                                    Brush outlineBrush = new SolidBrush(Color.Black);
+                                    float fontNum = image.Width / 9;
+                                    float outlineWidth = image.Width / 45;
+                                    System.Drawing.Font font = new System.Drawing.Font("黑体", fontNum);
+                                    Pen outlintPen = new Pen(Color.Black, outlineWidth);
+                                    int x_cn = (image.Width - (int)graphics.MeasureString(cn, font).Width) / 2;
+                                    int x_mNum = (image.Width - (int)graphics.MeasureString(mNum, font).Width) / 2;
+                                    int y_cn = (image.Height - (int)graphics.MeasureString(cn, font).Height) / 2;
+                                    int y_mNum = y_cn + (int)graphics.MeasureString(cn, font).Height;
+                                    for (float p = -outlineWidth; p <= outlineWidth; p += 0.5f)
+                                    {
+                                        graphics.DrawString(cn, font, outlineBrush, x_cn + p, y_cn + p);
+                                        graphics.DrawString(mNum, font, outlineBrush, x_mNum + p, y_mNum + p);
+                                    }
+                                    graphics.DrawString(cn, font, brush, x_cn, y_cn);
+                                    graphics.DrawString(mNum, font, brush, x_mNum, y_mNum);
+                                    graphics.Dispose();
+                                    MemoryStream ms = new MemoryStream();
+                                    image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+                                    MessageBody mb3 = SayPhoto(ms);
+                                    await EventArgs.Sender.SendPrivateMessage(mb3);
+                                    memberState[i, 1] = "1.1";
+                                    MessageBody mb4 = SayTextMessage("已完成处理。可以发送下一组cn和m码或发送“结束”退出当前状态");
+                                    await EventArgs.Sender.SendPrivateMessage(mb4);
+                                    break;
+                                }
+                        }
+                    }
+                }
+
                 //判断文本类型
                 char first = rawMessage[0];
-                //【.】命令入口
+
+
+                //无提示词聊天
+                if (Chat(ref rawMessage) == true)
+                {
+                    await EventArgs.Sender.SendPrivateMessage(rawMessage);
+                }
+
+                //【.】常规命令入口
                 if (first == char.Parse("."))
                 {
                     //提取内容
@@ -112,6 +272,37 @@ namespace ConsoleApp3
                         }
                     }
                 }
+
+                //【/】业务命令入口
+                if (first == char.Parse("/"))
+                {
+                    //标cn
+                    if (rawMessage.Remove(0, 1) == "标cn")
+                    {
+                        string state = "0";
+                        int path = 0;
+                        for (; path < 10; path++)
+                        {
+                            if (memberState[path, 0] == null)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                //需要添加缓冲区溢出语句
+                                break;
+                            }
+                        }
+                        memberState[path, 0] = EventArgs.SenderInfo.UserId.ToString();
+                        memberState[path, 1] = "1.1";
+                        MessageBody mb = SayTextMessage("已进入标记cn功能，请发送cn和m码，用“，”分隔。\n发送“结束”退出当前状态");
+                        await EventArgs.Sender.SendPrivateMessage(mb);
+                    }
+
+                }
+
+
+
             };
 
             service.Event.OnGroupMessage += async (sender, EventArgs) =>  //群消息处理
@@ -207,19 +398,108 @@ namespace ConsoleApp3
         {
             switch (message)
             {
+                //////早上好
                 case "早上好":
                     {
-                        message = "早上好……啊！已经早上了吗";
+                        if (GetRanNum(1, 3, out int num) == true)
+                        {
+                            switch (num)
+                            {
+                                case -1 or 1:
+                                    {
+                                        message = "已经是早上了吗，曲子还差一点，再努力会吧。";
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        message = "早上好，我刚写好一首demo，你能听听吗？想知道你的感想。";
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        message = "早。啊不用担心，曲子快写完了，我结尾完就会去小睡一会，真的。";
+                                        break;
+                                    }
+                            }
+                        }
                         return true;
                     }
+                //////晚上好
                 case "晚上好":
                     {
-                        message = "晚上了吗……我刚睡醒欸";
+                        if (GetRanNum(1, 3, out int num) == true)
+                        {
+                            switch (num)
+                            {
+                                case -1 or 1:
+                                    {
+                                        message = "欸？晚上了吗，嗯我刚醒。";
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        message = "晚上好，我打算一会去看看冰箱里，望月小姐给我留了什么，重新加热一下。";
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        message = "到了晚上变得有点凉呢，泡点果茶或者花茶喝，暖暖身子，顺便提提神吧。";
+                                        break;
+                                    }
+                            }
+                        }
                         return true;
                     }
+                //////吃了吗
                 case "吃了吗":
                     {
-                        message = "我在吃，你呢";
+                        if (GetRanNum(1, 3, out int num) == true)
+                        {
+                            switch (num)
+                            {
+                                case -1 or 1:
+                                    {
+                                        message = "欸，已经这个点了吗？一会去弄点泡面吃吧。";
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        message = "(怕打扰到别人的奏宝关闭了麦克风嗦面中)";
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        message = "嗯，望月小姐做的饭菜很好吃，你也不要忘了好好吃饭。";
+                                        break;
+                                    }
+                            }
+                        }
+                        return true;
+                    }
+                //////喜欢曲子
+                case "喜欢你的曲子":
+                    {
+                        if (GetRanNum(1, 3, out int num) == true)
+                        {
+                            switch (num)
+                            {
+                                case -1 or 1:
+                                    {
+                                        message = "谢谢，下次我也会努力的，希望你还会喜欢。";
+                                        break;
+                                    }
+                                case 2:
+                                    {
+                                        message = "谢谢，如果这首歌能触及你的心底就好了。";
+                                        break;
+                                    }
+                                case 3:
+                                    {
+                                        message = "嗯，我好开心，但这还不够，下一次的曲子一定能够……";
+                                        break;
+                                    }
+                            }
+                        }
                         return true;
                     }
                 case "h":
@@ -341,7 +621,7 @@ namespace ConsoleApp3
             }
         }
 
-        static bool Link(string message, out string text)
+        static bool Link(string message, out string text)  //链接内容
         {
             text = "";
             switch (message.Remove(0, 1))
@@ -373,6 +653,7 @@ namespace ConsoleApp3
                     };
             };
         }
+
         #endregion
 
         #region  方法：构建消息体
@@ -385,7 +666,6 @@ namespace ConsoleApp3
             return reply;
         }
 
-
         static MessageBody ReplyTextMessage(string text, int messageId)  //纯文本文字，引用原文
         {
             MessageBody reply = new MessageBody(new List<SoraSegment>()
@@ -396,25 +676,46 @@ namespace ConsoleApp3
             return reply;
         }
 
-
-        static MessageBody ReplyPhoto(System.Drawing.Image photo)   //构建回复：单一图片
+        static MessageBody SayPhoto(MemoryStream ms)   //构建回复：单一图片
         {
-            //将image转为base64流
-            string photoStr = "";
-            MemoryStream memoryStream = new MemoryStream();
-            photo.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
-            byte[] arr = new byte[memoryStream.Length];
-            memoryStream.Position = 0;
-            memoryStream.Read(arr, 0, (int)memoryStream.Length);
-            memoryStream.Close();
-            photoStr = Convert.ToBase64String(arr);
-            //构建回复体
             MessageBody reply = new MessageBody(new List<SoraSegment>()
             {
-                SoraSegment.Image(photoStr),
+                SoraSegment.Image(ms),
             });
             return reply;
         }
+
+        #endregion
+
+        #region  方法：函数体
+
+        static bool GetRanNum(int min, int max, out int num)   //快速随机数生成器
+        {
+            num = -1;
+            Random random = new Random();
+            num = random.Next(min, max);
+            if (num == -1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        #endregion
+
+
+        #region  测试方法
+
+        /*
+        static Bitmap CreateNullPhoto(Bitmap photo)
+        {
+            Bitmap newPhoto = new Bitmap(photo.Width, photo.Height);
+            return newPhoto;
+        }
+        */
 
         #endregion
     }
